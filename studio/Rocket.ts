15dbd379c0c63d9b7b032b248308866aa5a7a03b
BeltedKingfisher <AmyJSeibert@gmail.com>
2021-06-18 14:53:00 -0500

    sumAdd still acting funny

---
+++ studio/Rocket.ts
@@ -0,0 +1,53 @@
+import { Astronaut } from './Astronaut';
+import { Payload } from './Payload';
+import { Cargo } from './Cargo';
+
+export class Rocket implements Payload {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo [];
+    astronauts: Astronaut [];
+    massKg: number;
+
+    constructor(name: string, totalCapacityKg: number){
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }//end of constructor
+
+    sumMass (items: Payload[]): number {
+        let sum: number = 0;
+        for (let i=0; i < items.length; i++) {
+            sum += items[i].massKg;
+        }
+        return sum;
+    }
+
+    currentMassKg(): number {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    }//end of currentMassKg
+
+    canAdd(item: Payload): boolean{
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg){
+            return true;
+        }else{
+            return false;
+        }//end of if statement
+    }//end of canAdd
+
+    addCargo(cargo: Cargo): boolean{
+        if (this.canAdd(cargo) === true){
+            return true;
+        }else{
+            return false;
+        }//end of if statement
+    }//end of addCargo
+
+    addAstronaut(astronaut: Astronaut): boolean{
+        if (this.canAdd(astronaut) === true){
+            return true;
+        }else{
+            return false;
+        }//end of if statement
+    }//end of addAstronaut
+
+}//end of class declaration
---
+++ studio/rocket.js
@@ -0,0 +1,45 @@
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    } //end of constructor
+    Rocket.prototype.sumMass = function (items) {
+        var sum = 0;
+        for (var i = 0; i < items.length; i++) {
+            sum += items[i].massKg;
+        }
+        return sum;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    }; //end of currentMassKg
+    Rocket.prototype.canAdd = function (item) {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+        else {
+            return false;
+        } //end of if statement
+    }; //end of canAdd
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo) === true) {
+            return true;
+        }
+        else {
+            return false;
+        } //end of if statement
+    }; //end of addCargo
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut) === true) {
+            return true;
+        }
+        else {
+            return false;
+        } //end of if statement
+    }; //end of addAstronaut
+    return Rocket;
+}()); //end of class declaration
+exports.Rocket = Rocket;
